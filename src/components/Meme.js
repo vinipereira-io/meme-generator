92d539503186dfad93a61f0b197e4efcb7a9be9f
Vini Pereira <vini@vinipereira.io>
2023-03-06 22:51:19 +1100

    change meme form to a div

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,12 @@
 function Meme() {
     return (
-        <form className='meme'>
+        <div className='meme'>
             <div className='meme--inputs'>
                 <input type='text' placeholder='Top text'></input>
                 <input type='text' placeholder='Bottom text'></input>
             </div>
             <button>Get a new meme image</button>
-        </form>
+        </div>
     )
 }
 
