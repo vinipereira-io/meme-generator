690b9cd9004ee174327689b569209284540b92d5
Vini Pereira <vini@vinipereira.io>
2023-03-07 20:41:14 +1100

    get a random url from memeData

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,17 @@
+import MemesData from '../memesData.js'
+
 function Meme() {
+    function getImage() {
+        console.log(MemesData.data.memes[Math.floor(Math.random() * 100)].url);
+    }
+
     return (
         <div className='meme'>
             <div className='meme--inputs'>
                 <input type='text' placeholder='Top text'></input>
                 <input type='text' placeholder='Bottom text'></input>
             </div>
-            <button>Get a new meme image</button>
+            <button onClick={getImage}>Get a new meme image</button>
         </div>
     )
 }
